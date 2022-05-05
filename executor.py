15bc72479e84635cc5fc75740ae318d1e5f98ba2
Mahin Bin Hasan <80515603+mahinbinhasan@users.noreply.github.com>
2022-05-05 10:14:01 +0600

    Update executor.py

--- executor.py
+++ executor.py
@@ -1,19 +1,5 @@
-exec("".join(map(chr,[int("".join(str({':(': 4,
- ':)': 0,
- ':/': 7,
- ':D': 1,
- ':P': 2,
- ':S': 3,
- ':{': 8,
- ';)': 9,
- '=)': 5,
- '=/': 6}[i]) for i in x.split())) for x in
-":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  :/ :/ \
- ;) :/  :D :) :(  :D :) =)  :D :D :)  :S :P  :/ :S  :D :D :)  :D :) :P\
-  :D :D :D  :S :P  :/ :P  :D :) =)  :D :) :)  :D :) :D  :D :D :(  :( =\
-/  :( =/  :S ;)  :( :D  :D :)  :D :) ;)  :S :P  =/ :D  :S :P  :S ;)  ;\
-) :/  :S ;)"
-.split("  ")])))
+print('Mahin Info Hider..')
+m = 'a'
 filename = input('Enter the file name with extension\n\n==>')
 f = open(filename,m)
 
